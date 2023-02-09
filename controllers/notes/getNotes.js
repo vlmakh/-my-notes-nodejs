47c509a4d4b78802f6601adca34f1fe91355830e
Vlad Makhnenko <vlmakh@gmail.com>
2023-02-09 19:31:32 +0200

    hide owner in getNotes

--- controllers/notes/getNotes.js
+++ controllers/notes/getNotes.js
@@ -4,7 +4,7 @@ const getNotes = async (req, res, next) => {
   const { _id } = await req.user;
 
   try {
-    const data = await Note.find({ owner: _id });
+    const data = await Note.find({ owner: _id }, { owner: 0 });
 
     res.json(data);
   } catch (error) {
